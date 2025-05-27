d38c2fd4eba8495ab35917f014d5f2f6f0adf1e4
Bailey Russo <baileyrusso3371@gmail.com>
2025-05-27 10:43:32 -0400

    Init

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// src/main.ts
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+
+createApp(App).use(vuetify).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }), // Enables automatic Vuetify component imports
+  ],
+})
